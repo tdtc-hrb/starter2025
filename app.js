2d144211758a532ff52affb146345f1fbfdc7e00
tdtc-hrb <tdtc.hrb@gmail.com>
2025-02-20 06:39:28 -0800

    add thousand separator

--- app.js
+++ app.js
@@ -11,23 +11,57 @@ const myCounter = document.getElementById('counter');
 store.subscribe(
     () =>
     {
-        let displayFormat = store.getState();
+        let displayFormat = thousand_separator( store.getState() );
         myCounter.innerText = displayFormat;
     }
 );
 
 let iCounter = 1;
+let iMs = 200;
 
 console.log("Redux started");
 
 setInterval(
     () => {
-        console.log("1000 ms x " + iCounter);
+        console.log(iMs + " ms x " + iCounter);
 
         // update state
         store.dispatch({ type: 'INC' });
 
         iCounter++;
     },
-    500
+    iMs
 );
+
+function thousand_separator(cny) {
+    let strCny = cny.toString();
+    let array = strCny.split("");
+    let strNum = "";
+    let strDec = "";
+
+    let result = "";
+
+    let iLenCny = strCny.length;
+    let index = strCny.indexOf(".");
+    if ( index > 0) {
+        strNum = strCny.substring(0, index);
+        strDec = strCny.substr(index - iLenCny);
+
+        strCny = strNum;
+    }
+
+    let len = strCny.length;
+    let count = 0;
+    for (let j = len; j > 0; j--) {
+        count++;
+        if (count % 4 == 0) {
+            array.splice(j, 0, ",");
+            count++;
+            len++;
+        }
+    }
+
+    result = array.join("") + strDec;
+
+    return result;
+}
